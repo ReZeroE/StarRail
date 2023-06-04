25cd9351c53ea44edd2bcd21cc6ee14b4b6b443e
Kevin L <68928036+ReZeroE@users.noreply.github.com>
2023-06-04 05:34:12 -0400

    Cleaned up temp testing code snippets

--- tests/test_starrail.py
+++ tests/test_starrail.py
@@ -28,32 +28,3 @@ import time
 import os
 import time
 import pyautogui
-from elevate import elevate
-
-
-import ctypes
-import os
-
-try:
-    is_admin = os.getuid() == 0
-except AttributeError:
-    is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-
-print(is_admin)
-
-# from typing import Optional
-# from ctypes import wintypes, windll, create_unicode_buffer
-
-# def getForegroundWindowTitle() -> Optional[str]:
-#     hWnd = windll.user32.GetForegroundWindow()
-#     length = windll.user32.GetWindowTextLengthW(hWnd)
-#     buf = create_unicode_buffer(length + 1)
-#     windll.user32.GetWindowTextW(hWnd, buf, length + 1)
-    
-#     # 1-liner alternative: return buf.value if buf.value else None
-#     if buf.value:
-#         return buf.value
-#     else:
-#         return None
-    
-# print(getForegroundWindowTitle())
